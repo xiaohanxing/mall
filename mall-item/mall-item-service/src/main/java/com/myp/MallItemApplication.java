37b9770eeab3a36205125b24bb029c95ca92e6c7
xiaohanxing <18235111810@163.com>
2020-07-26 00:53:06 +0800

    商城后端框架初始化

---
+++ mall-item/mall-item-service/src/main/java/com/myp/MallItemApplication.java
@@ -0,0 +1,13 @@
+package com.myp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MallItemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallItemApplication.class);
+    }
+}
